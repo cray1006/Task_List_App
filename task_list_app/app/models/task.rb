731d339c7ced6cd7ee285fba3ae10f5a604d4982
cray1006 <cray@nd.edu>
2015-11-09 13:36:54 -0500

    added 1-10 range for Task Priority

--- task_list_app/app/models/task.rb
+++ task_list_app/app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
   belongs_to :task_list
   
   validates :Title, presence: true
-  validates :Priority, presence: true
+  validates :Priority, presence: true, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
   validates :DueDate, presence: true
   validates :task_list_id, presence: true
 end
