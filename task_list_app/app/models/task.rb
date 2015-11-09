2d8a8a281dd7a1cde095a69906c1b308c27c2f9c
cray1006 <cray@nd.edu>
2015-11-09 13:29:47 -0500

    added Title, Priority, DueDate, and task_list_id validations to Task

--- task_list_app/app/models/task.rb
+++ task_list_app/app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ActiveRecord::Base
   belongs_to :task_list
+  
+  validates :Title, presence: true
+  validates :Priority, presence: true
+  validates :DueDate, presence: true
+  validates :task_list_id, presence: true
 end
