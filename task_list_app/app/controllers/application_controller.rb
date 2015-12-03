b4ea3a65913c7e1fd349e23d86fec0605f01cbfb
cray1006 <cray@nd.edu>
2015-12-02 20:59:43 -0500

    app now displays task lists in sidebar

--- task_list_app/app/controllers/application_controller.rb
+++ task_list_app/app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   layout "todo_list"
+  
+  before_action :get_all_task_lists
+  
+  def get_all_task_lists
+    @all_task_lists = TaskList.all
+  end
+    
 end
